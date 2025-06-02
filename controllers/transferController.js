7bc4505b25eadd08da5efa8e5a063988879c512f
Biswaraj Choudhury <76588598+captraj@users.noreply.github.com>
2025-06-03 01:19:12 +0530

    Implement transfer fetching functions in transferController (get fns)

---
+++ controllers/transferController.js
@@ -0,0 +1,21 @@
+const Transfer = require('../models/Transfer');
+const mongoose = require('mongoose');
+
+
+//Fetch all the Transfers
+const getTransfers = async (req, res) => {
+    const transfers = await Transfer.find({}).sort({createdAt: -1})
+    res.status(200).json(transfers)
+};
+
+//Fetch a particular Transfer
+
+const getTransfer = async (req, res) => {
+    const {id} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({ error: 'No such transfer found!'})
+    }
+
+    return res.status(200).json(await Transfer.findById(id));
+}
